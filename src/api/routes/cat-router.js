23fc0df0cedc0c96814df3bc2f47beb1870f69ad
TheZink <ilkka.sinkonen@gmail.com>
2025-04-05 05:51:38 +0300

    Updated router

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {getCat,getCatById,postCat,putCat,deleteCat,
+import {listAllCats,getCatById,postCat,updateCat,deleteCat,
 } from '../controllers/cat-controller.js';
 
 const catRouter = express.Router();
 
-catRouter.route('/').get(getCat).post(postCat);
+catRouter.route('/').get(listAllCats).post(postCat);
 
-catRouter.route('/:id').get(getCatById).put(putCat).delete(deleteCat);
+catRouter.route('/:id').get(getCatById).put(updateCat).post(postCat).delete(deleteCat);
 
 export default catRouter;
