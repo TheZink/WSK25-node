70a0248bb36edd5c52ffd62418eea30e3f3d42ec
TheZink <ilkka.sinkonen@gmail.com>
2025-04-05 05:46:01 +0300

    Updated Cat controllers

--- src/api/controllers/cat-controller.js
+++ src/api/controllers/cat-controller.js
@@ -1,4 +1,4 @@
-import { addCat, findCatById, listAllCats } from "../models/cat-model";
+import { addCat, findCatById, listAllCats, updateCat, removeCat } from "../models/cat-model";
 
 const getCat = (req, res) => {
     res.json(listAllCats());
@@ -23,12 +23,34 @@ const postCat = (req, res) => {
     }
 }
 
-const putCat = (res, req) => {
-    //TODO
-    res.sendStatus(200);
+const putCat = (req, res) => {
+    const catId = req.params.id;
+    const updatedCat = req.body;
+
+    const result = updateCat(catId, updatedCat);
+
+    if(result){
+        res.sendStatus(200);
+        res.json(result)
+    } else {
+        res.sentStatus(400);
+    }
+
 }
 
 const deleteCat = (req, res) => {
-    //TODO
+    const catId = req.params.id;
+
+    const result = removeCat(catId);
+   
+    if(result){
+        res.sendStatus(200);
+        res.json(result)
+    } else {
+        res.sentStatus(400);
+    }
+
     res.sendStatus(200);
 }
+
+export {getCat, getCatById, postCat, putCat, deleteCat}
