3b3f4853c86011d4058b71e142b7628db215f74c
TheZink <ilkka.sinkonen@gmail.com>
2025-04-05 05:52:41 +0300

    Modified routers

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import catRouter from './routes/cat-router';
+import userRouter from './routers/users-router';
 
 const router = express.Router();
 
 router.use('/cats', catRouter);
+router.use('/users', userRouter);
 
 export default router;
