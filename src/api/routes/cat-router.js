c05a6129c734c2aac7db9ec863bcc416cf1fef98
TheZink <ilkka.sinkonen@gmail.com>
2025-04-05 06:08:02 +0300

    Fixed routes issue

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {listAllCats,getCatById,postCat,updateCat,deleteCat,
+import {getCat,getCatById,postCat,putCat,deleteCat,
 } from '../controllers/cat-controller.js';
 
 const catRouter = express.Router();
 
-catRouter.route('/').get(listAllCats).post(postCat);
+catRouter.route('/').get(getCat).post(postCat);
 
-catRouter.route('/:id').get(getCatById).put(updateCat).post(postCat).delete(deleteCat);
+catRouter.route('/:id').get(getCatById).put(putCat).post(postCat).delete(deleteCat);
 
 export default catRouter;
