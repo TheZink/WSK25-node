6f0ebae1bd3335950c3061554f9d166e338e3c69
TheZink <ilkka.sinkonen@gmail.com>
2025-04-07 18:45:40 +0300

    Added sharp to the cat-routes

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import multer from 'multer';
+import createThumbnail from '../../middlewares.js';
+
 import {getCat,getCatById,postCat,putCat,deleteCat,
 } from '../controllers/cat-controller.js';
 
@@ -7,7 +9,7 @@ const catRouter = express.Router();
 
 const upload = multer({dest: 'uploads/'});
 
-catRouter.route('/').get(getCat).post(upload.single('file'), postCat);
+catRouter.route('/').get(getCat).post(upload.single('file'), createThumbnail, postCat);
 
 catRouter.route('/:id').get(getCatById).put(putCat).post(postCat).delete(deleteCat);
 
