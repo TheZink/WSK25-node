2f2d19424afc5dde031e474060ded6bc453f0a30
TheZink <ilkka.sinkonen@gmail.com>
2025-04-08 04:59:18 +0300

    Createt auth-routes.js

--- src/api/routes/auth-routes.js
+++ src/api/routes/auth-routes.js
@@ -1,10 +1,5 @@
 import express from 'express';
-import {
-  getUser,
-  getUserById,
-  postUser,
-  putUser,
-  deleteUser,
+import {getUser, getUserById, postUser, putUser, deleteUser,
 } from '../controllers/user-controller.js';
 
 const userRouter = express.Router();
